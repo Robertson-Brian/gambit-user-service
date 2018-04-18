a181b942a4e75c509be57d127ca62e18d5613204
Joseph <30915062+Xeroph@users.noreply.github.com>
2018-04-18 15:57:18 -0400

    Removed excess loggers from ControlImpl

--- src/main/java/com/revature/gambit/controllers/TraineeControllerImpl.java
+++ src/main/java/com/revature/gambit/controllers/TraineeControllerImpl.java
@@ -14,14 +14,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.gambit.entities.Trainee;
 import com.revature.gambit.services.TraineeService;
-import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
-
 /**
  * Handles all Janus requests for Trainee resources.
  *
