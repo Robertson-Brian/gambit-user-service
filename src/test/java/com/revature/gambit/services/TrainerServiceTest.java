14c15b60d45864dec66f8d83b24ad1ca4812539c
Junyu Chen <chenjunyu1005@gmail.com>
2018-04-18 11:31:08 -0400

    adding test method

---
+++ src/test/java/com/revature/gambit/services/TrainerServiceTest.java
@@ -0,0 +1,32 @@
+package com.revature.gambit.services;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.revature.gambit.entities.Trainer;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrainerServiceTest {
+	
+	private static final Logger log = Logger.getLogger(TrainerServiceTest.class);
+	
+	@Autowired
+	private TrainerService trainerService; 
+	
+	@Test
+	public void findTrainerByIdRestTest(){
+		log.trace("Testing findTrainerById with Rest");
+		
+
+      		
+		
+		
+	}
+
+
+}
