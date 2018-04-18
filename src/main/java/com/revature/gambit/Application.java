d2d827c04e7767a59d8702e6ecf928488599878a
Joseph <30915062+Xeroph@users.noreply.github.com>
2018-04-18 10:27:08 -0400

    Removed comments

--- src/main/java/com/revature/gambit/Application.java
+++ src/main/java/com/revature/gambit/Application.java
@@ -2,7 +2,7 @@ package com.revature.gambit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import springfox.documentation.builders.PathSelectors;
@@ -11,9 +11,9 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-//@EnableSwagger2
+@EnableSwagger2
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
