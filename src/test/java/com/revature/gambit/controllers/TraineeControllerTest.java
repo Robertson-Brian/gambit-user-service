1c269843322af6ded282ba4c7bf4d60d30a3e8ee
Alex I <aiparrag3@gmail.com>
2018-04-18 13:10:12 -0400

    Added a getAll test to TraineeController

--- src/test/java/com/revature/gambit/controllers/TraineeControllerTest.java
+++ src/test/java/com/revature/gambit/controllers/TraineeControllerTest.java
@@ -1,10 +1,13 @@
 package com.revature.gambit.controllers;
 
+import static io.restassured.RestAssured.when;
+
 import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -12,11 +15,19 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class TraineeControllerTest {
 
 	private static final Logger log = Logger.getLogger(TraineeControllerTest.class);
-
+	
+	private static final String BASE_URL = "http://localhost:10000/trainees";
+	
 	@Autowired
 	private TraineeController traineeController;
 	
 	@Test
 	public void empty() {
+	}
+	
+	@Test
+	public void getAll(){
+		when().get(BASE_URL).then().assertThat()
+				.statusCode(HttpStatus.OK.value());
 	}
 }
