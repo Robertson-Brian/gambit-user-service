7168615541de322961669039ac10f30bd2e5e04a
shaleen <shaleen.anwar@gmail.com>
2018-04-18 13:43:38 -0400

    RestAssured API Controller test complete

--- src/test/java/com/revature/gambit/controllers/TraineeControllerTest.java
+++ src/test/java/com/revature/gambit/controllers/TraineeControllerTest.java
@@ -1,10 +1,14 @@
 package com.revature.gambit.controllers;
 
 import org.apache.log4j.Logger;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -14,4 +18,16 @@ public class TraineeControllerTest {
 
 	@Autowired
 	private TraineeController traineeController;
+
+	@Test
+	public void save() {
+		String body = "{\"userId\" : 0,\"firstName\": \"Shaleen\",\"lastName\": \"Anwar\",\"email\": \"shaleen.anwar@gmail.com\"}";
+		given().header("Content-Type", "application/json")
+				.body(body)
+				.when()	
+				.post("http://localhost:10001/trainees/").then().assertThat().statusCode(201);
+
+	}
+	
+
 }
