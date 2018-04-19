7044cadc177a78ecb5964c1b25eb75bdbd565642
Mark Fleres <markfleres@gmail.com>
2018-04-19 11:35:36 -0400

    Removed extra imports

--- src/test/java/com/revature/gambit/services/TrainerServiceTest.java
+++ src/test/java/com/revature/gambit/services/TrainerServiceTest.java
@@ -5,10 +5,7 @@ import static org.junit.Assert.assertNotEquals;
 
 import org.apache.log4j.Logger;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.revature.gambit.GambitTest;
 import com.revature.gambit.entities.Trainer;
