1a4b6d541032b8498dd0c3db9bbb816bd16f78df
Mark Fleres <markfleres@gmail.com>
2018-04-19 15:27:48 -0400

    made the register trainer tests more granular, removed the @Before in TrainerServiceTest that was causing code failure, and fixed the indentation

--- src/test/java/com/revature/gambit/controllers/TrainerControllerTest.java
+++ src/test/java/com/revature/gambit/controllers/TrainerControllerTest.java
@@ -31,89 +31,94 @@ public class TrainerControllerTest extends GambitTest {
     private static final String FIND_ALL_TRAINER_TITLES_URI = BASE_URI + "/titles";
     private static final String FIND_ALL_TRAINERS_URI = BASE_URI;
     private static final String REGISTER_TRAINER_URI = BASE_URI;
-    private static final String FIND_TRAINER_BY_EMAIL_URL = BASE_URI;
 
     @Test
     public void testDeleteTrainer() {
-	log.info("Deleting a Trainer");
-	short trainerId = 1;
-	given().port(port).delete(BASE_URI + "/{id}", trainerId).then().assertThat().statusCode(HttpStatus.OK_200);
+		log.info("Deleting a Trainer");
+		short trainerId = 1;
+		given().port(port).delete(BASE_URI + "/{id}", trainerId).then().assertThat().statusCode(HttpStatus.OK_200);
     }
 
     @Test
     public void testDeleteNonexistentTrainer() {
-	log.info("Deleting a Trainer");
-	short trainerId = -1;
-	given().port(port).delete(BASE_URI + "/{id}", trainerId).then().assertThat()
-		.statusCode(HttpStatus.INTERNAL_SERVER_ERROR_500);
+		log.info("Deleting a Trainer");
+		short trainerId = -1;
+		given().port(port).delete(BASE_URI + "/{id}", trainerId).then().assertThat()
+			.statusCode(HttpStatus.INTERNAL_SERVER_ERROR_500);
     }
 
     @Test
     public void findTrainerByEmail200() {
-	log.debug("test findTrainerByEmail with bad input");
-	String email = "steven.kelsey@revature.com";
-	given().when().port(port).get(FIND_TRAINER_BY_EMAIL_URI, email).then().assertThat()
-		.statusCode(HttpStatus.OK_200).body("email", equalTo(email));
+		log.debug("test findTrainerByEmail with bad input");
+		String email = "steven.kelsey@revature.com";
+		given().when().port(port).get(FIND_TRAINER_BY_EMAIL_URI, email).then().assertThat()
+			.statusCode(HttpStatus.OK_200).body("email", equalTo(email));
 
     }
 
     @Test
     public void findTrainerByEmail500() {
-	log.debug("test findTrainerByEmail with bad input");
-	String email = "sdjkssx@gmail.com";
-	String body = given().when().port(port).get(FIND_TRAINER_BY_EMAIL_URI, email).then().assertThat()
-		.statusCode(HttpStatus.OK_200).extract().body().asString();
-
-	assertEquals(body, "");
+		log.debug("test findTrainerByEmail with bad input");
+		String email = "sdjkssx@gmail.com";
+		String body = given().when().port(port).get(FIND_TRAINER_BY_EMAIL_URI, email).then().assertThat()
+			.statusCode(HttpStatus.OK_200).extract().body().asString();
+	
+		assertEquals(body, "");
     }
 
     @Test
     public void testFindAllTitles() {
-	log.debug("Find all trainers titles at : " + FIND_ALL_TRAINER_TITLES_URI);
-
-	given().port(port).basePath(FIND_ALL_TRAINER_TITLES_URI).when().get().then().assertThat()
-		.statusCode(HttpStatus.OK_200).body("$", hasItems("Lead Trainer", "Vice President of Technology",
-			"Technology Manager", "Senior Java Developer", "Trainer", "Senior Trainer"));
+		log.debug("Find all trainers titles at : " + FIND_ALL_TRAINER_TITLES_URI);
+	
+		given().port(port).basePath(FIND_ALL_TRAINER_TITLES_URI).when().get().then().assertThat()
+			.statusCode(HttpStatus.OK_200).body("$", hasItems("Lead Trainer", "Vice President of Technology",
+				"Technology Manager", "Senior Java Developer", "Trainer", "Senior Trainer"));
 
     }
 
     @Test
     public void findTrainerByEmailNonTrainer() {
-	log.debug("test findTrainerByEmail with non trainer email.");
-	String email = "ychenq001@gmail.com";
-	String body = given().when().port(port).get(FIND_TRAINER_BY_EMAIL_URI, email).then().assertThat()
-		.statusCode(HttpStatus.OK_200).extract().body().asString();
-
-	assertEquals(body, "");
+		log.debug("test findTrainerByEmail with non trainer email.");
+		String email = "ychenq001@gmail.com";
+		String body = given().when().port(port).get(FIND_TRAINER_BY_EMAIL_URI, email).then().assertThat()
+			.statusCode(HttpStatus.OK_200).extract().body().asString();
+	
+		assertEquals(body, "");
     }
 
     @SuppressWarnings("unchecked")
     @Test
     public void testFindAllTrainers() {
-	log.debug("Find all trainers titles at : " + FIND_ALL_TRAINERS_URI);
-
-	List<Trainer> trainers = new ArrayList<>();
-
-	trainers = given().port(port).basePath(FIND_ALL_TRAINERS_URI).when().get().then().assertThat()
-		.statusCode(HttpStatus.OK_200).extract().body().as(trainers.getClass());
-
-	assertTrue(!trainers.isEmpty());
+		log.debug("Find all trainers titles at : " + FIND_ALL_TRAINERS_URI);
+	
+		List<Trainer> trainers = new ArrayList<>();
+	
+		trainers = given().port(port).basePath(FIND_ALL_TRAINERS_URI).when().get().then().assertThat()
+			.statusCode(HttpStatus.OK_200).extract().body().as(trainers.getClass());
+	
+		assertTrue(!trainers.isEmpty());
     }
 
     @Test
     public void testRegisterTrainer() {
-	Trainer newTrainer = new Trainer("Mark", "Fleres", "mfleres@gmail.com", "Trainer");
-
-	given().contentType(ContentType.JSON).body(newTrainer).when().port(port).post(REGISTER_TRAINER_URI).then()
-		.assertThat().statusCode(HttpStatus.OK_200).and().contentType(ContentType.JSON).and()
-		.body("firstName", equalTo("Mark"));
-
-	// Test that a repeat register fails (email must be unique)
-	given().contentType(ContentType.JSON).body(newTrainer).when().port(port).post(REGISTER_TRAINER_URI).then()
-		.assertThat().statusCode(HttpStatus.BAD_REQUEST_400);
-
-	Trainer emptyTrainer = new Trainer("", "", "", "");
-	given().contentType(ContentType.JSON).body(emptyTrainer).when().port(port).post(REGISTER_TRAINER_URI).then()
-		.assertThat().statusCode(HttpStatus.BAD_REQUEST_400);
+		Trainer newTrainer = new Trainer("Mark", "Fleres", "mfleres@gmail.com", "Trainer");
+		given().contentType(ContentType.JSON).body(newTrainer).when().port(port).post(REGISTER_TRAINER_URI).then()
+			.assertThat().statusCode(HttpStatus.OK_200).and().contentType(ContentType.JSON).and()
+			.body("firstName", equalTo("Mark"));
+    }
+    
+    @Test
+    public void testRegisterExistingTrainer() {
+    	// Test that a repeat register fails (email must be unique)
+    	Trainer repeatTrainer = new Trainer("Patrick","Walsh","patrick.walsh@revature.com","Trainer");
+		given().contentType(ContentType.JSON).body(repeatTrainer).when().port(port).post(REGISTER_TRAINER_URI).then()
+			.assertThat().statusCode(HttpStatus.BAD_REQUEST_400);
+    }
+    
+    @Test
+    public void testRegisterEmptyTrainer() {
+		Trainer emptyTrainer = new Trainer("", "", "", "");
+		given().contentType(ContentType.JSON).body(emptyTrainer).when().port(port).post(REGISTER_TRAINER_URI).then()
+			.assertThat().statusCode(HttpStatus.BAD_REQUEST_400);
     }
 }
--- src/test/java/com/revature/gambit/services/TrainerServiceTest.java
+++ src/test/java/com/revature/gambit/services/TrainerServiceTest.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNull;
 
 import org.apache.log4j.Logger;
-import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -20,65 +19,62 @@ public class TrainerServiceTest extends GambitTest {
     @Autowired
     private TrainerService trainerService;
 
-    private Trainer trainer1;
-    private Trainer trainer2;
-
-    @Before
-    public void testCreateTrainers() {
-	trainer1 = new Trainer("John", "Doe", "JohnDoe@gmail.com", "Trainer");
-	trainer2 = new Trainer("Peter", "Alagna", "peter.alagna@revature.com", "Wizard");
-
-	trainerService.newTrainer(trainer1);
-	trainerService.newTrainer(trainer2);
-    }
-
     @Test
     public void testNewTrainer() {
-	log.debug("Testing trainerService.newTrainer(Trainer trainer)");
-	Trainer newTrainer = new Trainer("Mark", "Fleres", "mfleres@gmail.com", "Dr.");
-	log.trace("newTrainer = " + newTrainer);
-	Trainer savedTrainer = trainerService.newTrainer(newTrainer);
-	log.trace("savedTrainer = " + savedTrainer);
-	assertNotEquals(0, savedTrainer.getUserId());
-	assertEquals(newTrainer.getTitle(), savedTrainer.getTitle());
-	assertEquals(newTrainer.getFirstName(), savedTrainer.getFirstName());
-
-	// Test Repeat Trainer
-	Trainer savedRepeatTrainer = trainerService.newTrainer(newTrainer);
-	assertEquals(null, savedRepeatTrainer);
-
-	// Test Empty Trainer
-	Trainer emptyTrainer = new Trainer("", "", "", "");
-	Trainer savedEmptyTrainer = trainerService.newTrainer(emptyTrainer);
-	assertEquals(null, savedEmptyTrainer);
+		log.debug("Testing trainerService.newTrainer(Trainer trainer)");
+		Trainer newTrainer = new Trainer("Mark", "Fleres", "mfleres@gmail.com", "Dr.");
+		log.trace("newTrainer = " + newTrainer);
+		Trainer savedTrainer = trainerService.newTrainer(newTrainer);
+		log.trace("savedTrainer = " + savedTrainer);
+		assertNotEquals(0, savedTrainer.getUserId());
+		assertEquals(newTrainer.getTitle(), savedTrainer.getTitle());
+		assertEquals(newTrainer.getFirstName(), savedTrainer.getFirstName());
+    }
+    
+    @Test
+    public void testNewTrainerRepeat() {
+		// Test Repeat Trainer
+    	Trainer repeatTrainer = new Trainer("Patrick","Walsh","patrick.walsh@revature.com","Trainer");
+		Trainer savedRepeatTrainer = trainerService.newTrainer(repeatTrainer);
+		assertEquals(null, savedRepeatTrainer);
+    }
+    
+    @Test
+    public void testNewTrainerEmpty() {
+		// Test Empty Trainer
+		Trainer emptyTrainer = new Trainer("", "", "", "");
+		Trainer savedEmptyTrainer = trainerService.newTrainer(emptyTrainer);
+		assertEquals(null, savedEmptyTrainer);
     }
 
     @Test
     public void testDeleteTrainer() {
-	trainerService.delete(trainer1.getUserId());
-	assertNull(trainerService.findById(trainer1.getUserId()));
+    	Trainer trainer1 = new Trainer("John", "Doe", "JohnDoe@gmail.com", "Trainer");
+    	trainer1 = trainerService.newTrainer(trainer1);
+		trainerService.delete(trainer1.getUserId());
+		assertNull(trainerService.findById(trainer1.getUserId()));
     }
 
     @Test
     public void testDeleteNonexistentTrainer() {
-	assertThatThrownBy(() -> {
-	    trainerService.delete(-1);
-	});
+		assertThatThrownBy(() -> {
+		    trainerService.delete(-1);
+		});
     }
 
     @Test
     public void testGetAllTitles() {
 
-	log.debug("Testing trainerService.getAllTitles()");
-	assertEquals(6, trainerService.getAllTitles().size());
-	assertNotEquals(0, trainerService.getAllTitles().size());
+		log.debug("Testing trainerService.getAllTitles()");
+		assertEquals(6, trainerService.getAllTitles().size());
+		assertNotEquals(0, trainerService.getAllTitles().size());
     }
 
     @Test
     public void testGetAllTrainers() {
-	log.debug("Testing trainerService.getAll()");
-	assertEquals(12, trainerService.getAll().size());
-	assertNotEquals(0, trainerService.getAll().size());
+		log.debug("Testing trainerService.getAll()");
+		assertEquals(12, trainerService.getAll().size());
+		assertNotEquals(0, trainerService.getAll().size());
 	}
 
 	@Test
