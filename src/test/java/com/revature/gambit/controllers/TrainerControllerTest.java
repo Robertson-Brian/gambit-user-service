80a970e1ceaabac733c2b16071b66ed7b9247772
Junyu Chen <chenjunyu1005@gmail.com>
2018-04-19 16:11:18 -0400

    pass the test

--- src/test/java/com/revature/gambit/controllers/TrainerControllerTest.java
+++ src/test/java/com/revature/gambit/controllers/TrainerControllerTest.java
@@ -23,7 +23,7 @@ import org.apache.log4j.Logger;
 import org.eclipse.jetty.http.HttpStatus;
 
 import org.junit.Test;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
 
 
@@ -31,13 +31,15 @@ import org.springframework.boot.context.embedded.LocalServerPort;
 import com.revature.gambit.GambitTest;
 
 import com.revature.gambit.entities.Trainer;
-
-
+import com.revature.gambit.services.TrainerService;
 
 import io.restassured.http.ContentType;
 
 public class TrainerControllerTest extends GambitTest {
-
+   
+	
+	@Autowired
+    private TrainerService trainerService;
 
 	@LocalServerPort
 	private int port;
@@ -54,104 +56,199 @@ public class TrainerControllerTest extends GambitTest {
 	@Test
 	public void testFindTrainerById(){
 		log.debug("Testing findTrainerById ");
+	   int userId = trainerService.findTrainerByEmail("pjw6193@hotmail.com").getUserId();
 		given().
 		port(port).
+		contentType(ContentType.JSON).
 		when().
-		get(FIND_TRAINER_BY_ID_URI,2)
+		get(FIND_TRAINER_BY_ID_URI,userId)
 		.then()
 		.assertThat()
 		.statusCode(HttpStatus.OK_200)
 		.body("firstName", equalTo("Dan"),
-				"lastName" ,equalTo("Pickles"),
-				"email", equalTo("pjw6193@hotmail.com"),
-				"role.role",equalTo("ROLE_VP"),
-				"title", equalTo("Lead Trainer"));
-
-	}
-
-
-
-	@Test
-	public void testDeleteTrainer() {
-		log.info("Deleting a Trainer");
-		short trainerId = 1;
-		given().port(port).delete(BASE_URI + "/{id}", trainerId).then().assertThat().statusCode(HttpStatus.OK_200);
-	}
-
-	@Test
-	public void testDeleteNonexistentTrainer() {
-		log.info("Deleting a Trainer");
-		short trainerId = -1;
-		given().port(port).delete(BASE_URI + "/{id}", trainerId).then().assertThat()
-		.statusCode(HttpStatus.INTERNAL_SERVER_ERROR_500);
-	}
-
-	@Test
-	public void findTrainerByEmail200() {
-		log.debug("test findTrainerByEmail with bad input");
-		String email = "steven.kelsey@revature.com";
-		given().when().port(port).get(FIND_TRAINER_BY_EMAIL_URI, email).then().assertThat()
-		.statusCode(HttpStatus.OK_200).body("email", equalTo(email));
+			  "lastName" ,equalTo("Pickles"),
+			  "email", equalTo("pjw6193@hotmail.com"),
+			  "role.role",equalTo("ROLE_VP"),
+			  "title", equalTo("Lead Trainer"));
 
 	}
 
 	@Test
-	public void findTrainerByEmail500() {
-		log.debug("test findTrainerByEmail with bad input");
-		String email = "sdjkssx@gmail.com";
-		String body = given().when().port(port).get(FIND_TRAINER_BY_EMAIL_URI, email).then().assertThat()
-				.statusCode(HttpStatus.OK_200).extract().body().asString();
 
-		assertEquals(body, "");
-	}
+    public void testDeleteTrainer() {
 
-	@Test
-	public void testFindAllTitles() {
-		log.debug("Find all trainers titles at : " + FIND_ALL_TRAINER_TITLES_URI);
+    	log.debug("Deleting a Trainer");
 
-		given().port(port).basePath(FIND_ALL_TRAINER_TITLES_URI).when().get().then().assertThat()
-		.statusCode(HttpStatus.OK_200).body("$", hasItems("Lead Trainer", "Vice President of Technology",
-				"Technology Manager", "Senior Java Developer", "Trainer", "Senior Trainer"));
+    	int trainerId = trainerService.findTrainerByEmail("patrick.walsh@revature.com").getUserId();
 
-	}
+    	given().port(port).delete(BASE_URI + "/{id}", trainerId).then().assertThat().statusCode(HttpStatus.OK_200);
 
-	@Test
-	public void findTrainerByEmailNonTrainer() {
-		log.debug("test findTrainerByEmail with non trainer email.");
-		String email = "ychenq001@gmail.com";
-		String body = given().when().port(port).get(FIND_TRAINER_BY_EMAIL_URI, email).then().assertThat()
-				.statusCode(HttpStatus.OK_200).extract().body().asString();
+    }
 
-		assertEquals(body, "");
-	}
 
-	@SuppressWarnings("unchecked")
-	@Test
-	public void testFindAllTrainers() {
-		log.debug("Find all trainers titles at : " + FIND_ALL_TRAINERS_URI);
 
-		List<Trainer> trainers = new ArrayList<>();
+    @Test
 
-		trainers = given().port(port).basePath(FIND_ALL_TRAINERS_URI).when().get().then().assertThat()
-				.statusCode(HttpStatus.OK_200).extract().body().as(trainers.getClass());
+    public void testDeleteNonexistentTrainer() {
 
-		assertTrue(!trainers.isEmpty());
-	}
+    	log.debug("Deleting a Trainer");
 
-	@Test
-	public void testRegisterTrainer() {
-		Trainer newTrainer = new Trainer("Mark", "Fleres", "mfleres@gmail.com", "Trainer");
+    	int trainerId = -1;
 
-		given().contentType(ContentType.JSON).body(newTrainer).when().port(port).post(REGISTER_TRAINER_URI).then()
-		.assertThat().statusCode(HttpStatus.OK_200).and().contentType(ContentType.JSON).and()
-		.body("firstName", equalTo("Mark"));
+    	given().port(port).delete(BASE_URI + "/{id}", trainerId).then().assertThat()
 
-		// Test that a repeat register fails (email must be unique)
-		given().contentType(ContentType.JSON).body(newTrainer).when().port(port).post(REGISTER_TRAINER_URI).then()
-		.assertThat().statusCode(HttpStatus.BAD_REQUEST_400);
+    		.statusCode(HttpStatus.INTERNAL_SERVER_ERROR_500);
+
+    }
+
+
+
+    @Test
+
+    public void findTrainerByEmail200() {
+
+    	log.debug("test findTrainerByEmail with bad input");
+
+    	String email = "steven.kelsey@revature.com";
+
+    	given().when().port(port).get(FIND_TRAINER_BY_EMAIL_URI, email).then().assertThat()
+
+    		.statusCode(HttpStatus.OK_200).body("email", equalTo(email));
+
+
+
+    }
+
+
+
+    @Test
+
+    public void findTrainerByEmail500() {
+
+    	log.debug("test findTrainerByEmail with bad input");
+
+    	String email = "sdjkssx@gmail.com";
+
+    	String body = given().when().port(port).get(FIND_TRAINER_BY_EMAIL_URI, email).then().assertThat()
+
+    				.statusCode(HttpStatus.OK_200).extract().body().asString();
+
+	
+
+    	assertEquals(body, "");
+
+    }
+
+
+
+    @Test
+
+    public void testFindAllTitles() {
+
+    	log.debug("Find all trainers titles at : " + FIND_ALL_TRAINER_TITLES_URI);
+
+	
+
+    	given().port(port).basePath(FIND_ALL_TRAINER_TITLES_URI).when().get().then().assertThat()
+
+    		.statusCode(HttpStatus.OK_200).body("$", hasItems("Lead Trainer", "Vice President of Technology",
+
+    					"Technology Manager", "Senior Java Developer", "Trainer", "Senior Trainer"));
+
+
+
+    }
+
+
+
+    @Test
+
+    public void findTrainerByEmailNonTrainer() {
+
+    	log.debug("test findTrainerByEmail with non trainer email.");
+
+    	String email = "ychenq001@gmail.com";
+
+    	String body = given().when().port(port).get(FIND_TRAINER_BY_EMAIL_URI, email).then().assertThat()
+
+    				.statusCode(HttpStatus.OK_200).extract().body().asString();
+
+	
+
+    	assertEquals(body, "");
+
+    }
+
+
+
+    @SuppressWarnings("unchecked")
+
+    @Test
+
+    public void testFindAllTrainers() {
+
+    	log.debug("Find all trainers titles at : " + FIND_ALL_TRAINERS_URI);
+
+	
+
+    	List<Trainer> trainers = new ArrayList<>();
+
+	
+
+    	trainers = given().port(port).basePath(FIND_ALL_TRAINERS_URI).when().get().then().assertThat()
+
+    			.statusCode(HttpStatus.OK_200).extract().body().as(trainers.getClass());
+
+	
+
+    	assertTrue(!trainers.isEmpty());
+
+    }
+
+
+
+    @Test
+
+    public void testRegisterTrainer() {
+
+    	Trainer newTrainer = new Trainer("Mark", "Fleres", "mfleres@gmail.com", "Trainer");
+
+    	given().contentType(ContentType.JSON).body(newTrainer).when().port(port).post(REGISTER_TRAINER_URI).then()
+
+    		.assertThat().statusCode(HttpStatus.OK_200).and().contentType(ContentType.JSON).and()
+
+    		.body("firstName", equalTo("Mark"));
+
+    }
+
+    
+
+    @Test
+
+    public void testRegisterExistingTrainer() {
+
+    	// Test that a repeat register fails (email must be unique)
+
+    	Trainer repeatTrainer = new Trainer("Patrick","Walsh","patrick.walsh@revature.com","Trainer");
+
+		given().contentType(ContentType.JSON).body(repeatTrainer).when().port(port).post(REGISTER_TRAINER_URI).then()
+
+			.assertThat().statusCode(HttpStatus.BAD_REQUEST_400);
+
+    }
+
+    
+
+    @Test
+
+    public void testRegisterEmptyTrainer() {
 
 		Trainer emptyTrainer = new Trainer("", "", "", "");
+
 		given().contentType(ContentType.JSON).body(emptyTrainer).when().port(port).post(REGISTER_TRAINER_URI).then()
-		.assertThat().statusCode(HttpStatus.BAD_REQUEST_400);
-	}
+
+			.assertThat().statusCode(HttpStatus.BAD_REQUEST_400);
+
+    }
+
 }
--- src/test/java/com/revature/gambit/services/TrainerServiceTest.java
+++ src/test/java/com/revature/gambit/services/TrainerServiceTest.java
@@ -2,14 +2,8 @@ package com.revature.gambit.services;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.Assert.assertEquals;
-
-
-
 import static org.junit.Assert.assertNotEquals;
-
-
 import static org.junit.Assert.assertNull;
-
 
 import org.apache.log4j.Logger;
 import org.junit.Before;
@@ -30,94 +24,169 @@ public class TrainerServiceTest extends GambitTest {
     @Autowired
     private TrainerService trainerService;
 
-    private Trainer trainer1;
-    private Trainer trainer2;
-
-    @Before
-    public void testCreateTrainers() {
-	trainer1 = new Trainer("John", "Doe", "JohnDoe@gmail.com", "Trainer");
-	trainer2 = new Trainer("Peter", "Alagna", "peter.alagna@revature.com", "Wizard");
-
-	trainerService.newTrainer(trainer1);
-	trainerService.newTrainer(trainer2);
-    }
-
     @Test
+
     public void testNewTrainer() {
-	log.debug("Testing trainerService.newTrainer(Trainer trainer)");
-	Trainer newTrainer = new Trainer("Mark", "Fleres", "mfleres@gmail.com", "Dr.");
-	log.trace("newTrainer = " + newTrainer);
-	Trainer savedTrainer = trainerService.newTrainer(newTrainer);
-	log.trace("savedTrainer = " + savedTrainer);
-	assertNotEquals(0, savedTrainer.getUserId());
-	assertEquals(newTrainer.getTitle(), savedTrainer.getTitle());
-	assertEquals(newTrainer.getFirstName(), savedTrainer.getFirstName());
 
-	// Test Repeat Trainer
-	Trainer savedRepeatTrainer = trainerService.newTrainer(newTrainer);
-	assertEquals(null, savedRepeatTrainer);
+    	log.debug("Testing trainerService.newTrainer(Trainer trainer)");
 
-	// Test Empty Trainer
-	Trainer emptyTrainer = new Trainer("", "", "", "");
-	Trainer savedEmptyTrainer = trainerService.newTrainer(emptyTrainer);
-	assertEquals(null, savedEmptyTrainer);
+    	Trainer newTrainer = new Trainer("Mark", "Fleres", "mfleres@gmail.com", "Dr.");
+
+    	log.trace("newTrainer = " + newTrainer);
+
+    	Trainer savedTrainer = trainerService.newTrainer(newTrainer);
+
+    	log.trace("savedTrainer = " + savedTrainer);
+
+    	assertNotEquals(0, savedTrainer.getUserId());
+
+    	assertEquals(newTrainer.getTitle(), savedTrainer.getTitle());
+
+    	assertEquals(newTrainer.getFirstName(), savedTrainer.getFirstName());
+
     }
 
+    
+
     @Test
+
+    public void testNewTrainerRepeat() {
+
+    	// Test Repeat Trainer
+
+    	Trainer repeatTrainer = new Trainer("Patrick","Walsh","patrick.walsh@revature.com","Trainer");
+
+    	Trainer savedRepeatTrainer = trainerService.newTrainer(repeatTrainer);
+
+    	assertEquals(null, savedRepeatTrainer);
+
+    }
+
+    
+
+    @Test
+
+    public void testNewTrainerEmpty() {
+
+    	// Test Empty Trainer
+
+    	Trainer emptyTrainer = new Trainer("", "", "", "");
+
+    	Trainer savedEmptyTrainer = trainerService.newTrainer(emptyTrainer);
+
+    	assertEquals(null, savedEmptyTrainer);
+
+    }
+
+
+
+    @Test
+
     public void testDeleteTrainer() {
-	trainerService.delete(trainer1.getUserId());
-	assertNull(trainerService.findById(trainer1.getUserId()));
+
+    	int patrickId = trainerService.findTrainerByEmail("patrick.walsh@revature.com").getUserId();
+
+    	trainerService.delete(patrickId);
+
+    	assertNull(trainerService.findById(patrickId));
+
     }
 
+
+
     @Test
+
     public void testDeleteNonexistentTrainer() {
-	assertThatThrownBy(() -> {
-	    trainerService.delete(-1);
-	});
+
+    	assertThatThrownBy(() -> {
+
+    		trainerService.delete(-1);
+
+    	});
+
     }
 
+
+
     @Test
+
     public void testGetAllTitles() {
 
-	log.debug("Testing trainerService.getAllTitles()");
-	assertEquals(6, trainerService.getAllTitles().size());
-	assertNotEquals(0, trainerService.getAllTitles().size());
+    	log.debug("Testing trainerService.getAllTitles()");
+
+    	assertEquals(6, trainerService.getAllTitles().size());
+
+    	assertNotEquals(0, trainerService.getAllTitles().size());
+
     }
+
+
 
     @Test
+
     public void testGetAllTrainers() {
-	log.debug("Testing trainerService.getAll()");
-	assertEquals(12, trainerService.getAll().size());
-	assertNotEquals(0, trainerService.getAll().size());
+
+    	log.debug("Testing trainerService.getAll()");
+
+    	assertEquals(12, trainerService.getAll().size());
+
+    	assertNotEquals(0, trainerService.getAll().size());
+
 	}
 
+
+
 	@Test
+
 	public void testFindTrainerByEmail() {
+
 		log.debug("Testing trainerService.findTrainerByEmail with correct email address");
+
 		String expected = "steven.kelsey@revature.com";
+
 		Trainer trainer = trainerService.findTrainerByEmail("steven.kelsey@revature.com");
+
 		assertEquals(trainer.getEmail(), expected);
+
 	}
 
+
+
 	@Test
+
 	public void testFindTrainerByEmailInvalid() {
+
 		log.debug("Testing trainerService.findTrainerByEmail with invalid email address");
+
 		Trainer trainer = trainerService.findTrainerByEmail("fdjnfjdd@revature.com");
+
 		assertEquals(trainer, null);
+
 	}
 
+
+
 	@Test
+
 	public void testFindTrainerByEmailNonTrainer() {
+
 		log.debug("Testing trainerService.findTrainerByEmail with non-trainer email address");
+
 		Trainer trainer = trainerService.findTrainerByEmail("ychenq001@gmail.com");
+
 		assertEquals(trainer, null);
+
     }
+
+
 	
 	@Test																																																																																																		
 	public void findTrainerById(){
 		log.debug("Find trainer by id");
 		Trainer newTrainer = new Trainer("Patrick","Walsh","patrick.walsh@revature.com","Lead Trainer");
-		Trainer findById = trainerService.findById(1);
+		 int userId = trainerService.findTrainerByEmail("patrick.walsh@revature.com").getUserId();
+			Trainer findById = trainerService.findById(userId);
+
 		assertEquals(newTrainer.getFirstName(), findById.getFirstName());
 		assertEquals(newTrainer.getLastName(), findById.getLastName());
 		assertEquals(newTrainer.getEmail(), findById.getEmail());
