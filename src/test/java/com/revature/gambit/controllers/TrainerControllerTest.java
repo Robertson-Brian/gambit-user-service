23851c2c01824ebf9b25feea8e0c42b0bfe373c8
Mark Fleres <markfleres@gmail.com>
2018-04-19 15:44:18 -0400

    Fixed the intendation issue

--- src/test/java/com/revature/gambit/controllers/TrainerControllerTest.java
+++ src/test/java/com/revature/gambit/controllers/TrainerControllerTest.java
@@ -19,7 +19,7 @@ import com.revature.gambit.entities.Trainer;
 
 import io.restassured.http.ContentType;
 
-public class TrainerControllerTest extends GambitTest {
+public class TrainerControllerTest extends GambitTest {	
 
     private static final Logger log = Logger.getLogger(TrainerControllerTest.class);
 
@@ -34,77 +34,77 @@ public class TrainerControllerTest extends GambitTest {
 
     @Test
     public void testDeleteTrainer() {
-		log.info("Deleting a Trainer");
-		short trainerId = 1;
-		given().port(port).delete(BASE_URI + "/{id}", trainerId).then().assertThat().statusCode(HttpStatus.OK_200);
+    	log.info("Deleting a Trainer");
+    	short trainerId = 1;
+    	given().port(port).delete(BASE_URI + "/{id}", trainerId).then().assertThat().statusCode(HttpStatus.OK_200);
     }
 
     @Test
     public void testDeleteNonexistentTrainer() {
-		log.info("Deleting a Trainer");
-		short trainerId = -1;
-		given().port(port).delete(BASE_URI + "/{id}", trainerId).then().assertThat()
-			.statusCode(HttpStatus.INTERNAL_SERVER_ERROR_500);
+    	log.info("Deleting a Trainer");
+    	short trainerId = -1;
+    	given().port(port).delete(BASE_URI + "/{id}", trainerId).then().assertThat()
+    		.statusCode(HttpStatus.INTERNAL_SERVER_ERROR_500);
     }
 
     @Test
     public void findTrainerByEmail200() {
-		log.debug("test findTrainerByEmail with bad input");
-		String email = "steven.kelsey@revature.com";
-		given().when().port(port).get(FIND_TRAINER_BY_EMAIL_URI, email).then().assertThat()
-			.statusCode(HttpStatus.OK_200).body("email", equalTo(email));
+    	log.debug("test findTrainerByEmail with bad input");
+    	String email = "steven.kelsey@revature.com";
+    	given().when().port(port).get(FIND_TRAINER_BY_EMAIL_URI, email).then().assertThat()
+    		.statusCode(HttpStatus.OK_200).body("email", equalTo(email));
 
     }
 
     @Test
     public void findTrainerByEmail500() {
-		log.debug("test findTrainerByEmail with bad input");
-		String email = "sdjkssx@gmail.com";
-		String body = given().when().port(port).get(FIND_TRAINER_BY_EMAIL_URI, email).then().assertThat()
-			.statusCode(HttpStatus.OK_200).extract().body().asString();
+    	log.debug("test findTrainerByEmail with bad input");
+    	String email = "sdjkssx@gmail.com";
+    	String body = given().when().port(port).get(FIND_TRAINER_BY_EMAIL_URI, email).then().assertThat()
+    				.statusCode(HttpStatus.OK_200).extract().body().asString();
 	
-		assertEquals(body, "");
+    	assertEquals(body, "");
     }
 
     @Test
     public void testFindAllTitles() {
-		log.debug("Find all trainers titles at : " + FIND_ALL_TRAINER_TITLES_URI);
+    	log.debug("Find all trainers titles at : " + FIND_ALL_TRAINER_TITLES_URI);
 	
-		given().port(port).basePath(FIND_ALL_TRAINER_TITLES_URI).when().get().then().assertThat()
-			.statusCode(HttpStatus.OK_200).body("$", hasItems("Lead Trainer", "Vice President of Technology",
-				"Technology Manager", "Senior Java Developer", "Trainer", "Senior Trainer"));
+    	given().port(port).basePath(FIND_ALL_TRAINER_TITLES_URI).when().get().then().assertThat()
+    		.statusCode(HttpStatus.OK_200).body("$", hasItems("Lead Trainer", "Vice President of Technology",
+    					"Technology Manager", "Senior Java Developer", "Trainer", "Senior Trainer"));
 
     }
 
     @Test
     public void findTrainerByEmailNonTrainer() {
-		log.debug("test findTrainerByEmail with non trainer email.");
-		String email = "ychenq001@gmail.com";
-		String body = given().when().port(port).get(FIND_TRAINER_BY_EMAIL_URI, email).then().assertThat()
-			.statusCode(HttpStatus.OK_200).extract().body().asString();
+    	log.debug("test findTrainerByEmail with non trainer email.");
+    	String email = "ychenq001@gmail.com";
+    	String body = given().when().port(port).get(FIND_TRAINER_BY_EMAIL_URI, email).then().assertThat()
+    				.statusCode(HttpStatus.OK_200).extract().body().asString();
 	
-		assertEquals(body, "");
+    	assertEquals(body, "");
     }
 
     @SuppressWarnings("unchecked")
     @Test
     public void testFindAllTrainers() {
-		log.debug("Find all trainers titles at : " + FIND_ALL_TRAINERS_URI);
+    	log.debug("Find all trainers titles at : " + FIND_ALL_TRAINERS_URI);
 	
-		List<Trainer> trainers = new ArrayList<>();
+    	List<Trainer> trainers = new ArrayList<>();
 	
-		trainers = given().port(port).basePath(FIND_ALL_TRAINERS_URI).when().get().then().assertThat()
-			.statusCode(HttpStatus.OK_200).extract().body().as(trainers.getClass());
+    	trainers = given().port(port).basePath(FIND_ALL_TRAINERS_URI).when().get().then().assertThat()
+    			.statusCode(HttpStatus.OK_200).extract().body().as(trainers.getClass());
 	
-		assertTrue(!trainers.isEmpty());
+    	assertTrue(!trainers.isEmpty());
     }
 
     @Test
     public void testRegisterTrainer() {
-		Trainer newTrainer = new Trainer("Mark", "Fleres", "mfleres@gmail.com", "Trainer");
-		given().contentType(ContentType.JSON).body(newTrainer).when().port(port).post(REGISTER_TRAINER_URI).then()
-			.assertThat().statusCode(HttpStatus.OK_200).and().contentType(ContentType.JSON).and()
-			.body("firstName", equalTo("Mark"));
+    	Trainer newTrainer = new Trainer("Mark", "Fleres", "mfleres@gmail.com", "Trainer");
+    	given().contentType(ContentType.JSON).body(newTrainer).when().port(port).post(REGISTER_TRAINER_URI).then()
+    		.assertThat().statusCode(HttpStatus.OK_200).and().contentType(ContentType.JSON).and()
+    		.body("firstName", equalTo("Mark"));
     }
     
     @Test
