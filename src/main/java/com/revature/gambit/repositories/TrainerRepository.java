271ff9caba70eca32cbd8e257cbc79bd053412be
DESKTOP-EPQFQ6U\jingy <jingyu459510777@gmail.com>
2018-04-19 15:09:05 -0400

    remove @Transactional

--- src/main/java/com/revature/gambit/repositories/TrainerRepository.java
+++ src/main/java/com/revature/gambit/repositories/TrainerRepository.java
@@ -4,11 +4,9 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.revature.gambit.entities.Trainer;
 
-@Transactional
 public interface TrainerRepository extends JpaRepository<Trainer, Integer> {
 
     public Trainer findByUserId(int userId);
