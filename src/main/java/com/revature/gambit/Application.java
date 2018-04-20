e92ec72181973964f9ba99256a38b21d2341d697
Junyu Chen <chenjunyu1005@gmail.com>
2018-04-20 14:31:23 -0400

    testing commandlinerunner to get all trainer

--- src/main/java/com/revature/gambit/Application.java
+++ src/main/java/com/revature/gambit/Application.java
@@ -1,10 +1,24 @@
 package com.revature.gambit;
 
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+
+import com.revature.gambit.entities.Trainee;
+import com.revature.gambit.entities.Trainer;
+import com.revature.gambit.entities.User;
+import com.revature.gambit.services.TraineeService;
+import com.revature.gambit.services.TrainerService;
+import com.revature.gambit.services.UserService;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -16,8 +30,11 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCircuitBreaker
-public class Application {
-	
+public class Application implements CommandLineRunner {
+	  private static Logger log =Logger.getLogger(Application.class);
+
+	@Autowired
+	ApplicationContext context;
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
@@ -26,5 +43,25 @@ public class Application {
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
 				.apis(RequestHandlerSelectors.basePackage("com.revature.gambit")).paths(PathSelectors.any()).build();
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+//		UserService bean = context.getBean(UserService.class);
+//		List<User> allUsers = bean.getAllUsers();
+//		log.info("Getting All Users: "+allUsers);
+		
+		 TrainerService bean = context.getBean(TrainerService.class);
+		  List<Trainer> all = bean.getAll();
+		  log.info("Getting All tainer:" +all);
+		  log.info("Getting All tainer size :" +all.size());
+
+		  
+		  
+	  
+//	  TraineeService traineeService = context.getBean(TraineeService.class);
+//	  List<Trainee> getAllTrainee = traineeService.getAll();
+//		  log.info("Getting All trainee " +getAllTrainee );
+//          
 	}
 }
--- src/main/java/com/revature/gambit/monitoring/UserFallback.java
+++ src/main/java/com/revature/gambit/monitoring/UserFallback.java
@@ -1,5 +1,26 @@
 package com.revature.gambit.monitoring;
 
-public class UserFallback {
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.context.ApplicationContext;
+
+import com.revature.gambit.services.UserService;
+//@SpringBootApplication
+//@EnableCircuitBreaker
+
+public class UserFallback  {
+
+  private static Logger log =Logger.getLogger(UserFallback.class);
+//	@Autowired
+//	ApplicationContext context;
+//	public void init(){
+//	
+//		
+//	log.info(context.getBean(UserService.class).getAllUsers());
+//
+//	}
+
 
 }
