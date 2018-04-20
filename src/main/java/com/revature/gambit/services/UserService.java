579154095a19d0138563bd68501ee2b2b9df6194
Mark Fleres <markfleres@gmail.com>
2018-04-20 14:25:23 -0400

    Began integration. Finished Repo and Service layers

--- src/main/java/com/revature/gambit/services/UserService.java
+++ src/main/java/com/revature/gambit/services/UserService.java
@@ -3,6 +3,7 @@ package com.revature.gambit.services;
 import java.util.List;
 
 import com.revature.gambit.entities.User;
+import com.revature.gambit.entities.UserRole;
 
 public interface UserService {
 
@@ -19,6 +20,18 @@ public interface UserService {
 	User findUserById(Integer id);
 
 	User findByName(String firstName, String lastName);
+	
+	/**
+	 * Gets all Users of a particular role
+	 * 
+	 * @author Devin Dellamano
+	 * * Gambit Integrators:
+	 * * @author Mark Fleres
+	 * 
+	 * @return List of Users containing all entries related to a specific role
+	 * @param the UserRole we want to filter the search by
+	 */
+	List<User> findByRole(UserRole role);
 
 	/**
 	 * Users are not deleted, they are simply set to inactive.
