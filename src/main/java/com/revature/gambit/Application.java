90366ba0870a3e092aa6405babe99da589d17a58
jingyu <jingyu459510777@gmail.com>
2018-04-20 14:34:49 -0400

    implement commandlinerunner interface

--- src/main/java/com/revature/gambit/Application.java
+++ src/main/java/com/revature/gambit/Application.java
@@ -1,10 +1,17 @@
 package com.revature.gambit;
 
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+
+import com.revature.gambit.services.TrainerService;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -15,8 +22,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 @EnableCircuitBreaker
-public class Application {
+public class Application implements CommandLineRunner{
+	
+	private static final Logger log = Logger.getLogger(Application.class);
+	
+	@Autowired
+	ApplicationContext context;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -26,5 +39,13 @@ public class Application {
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
 				.apis(RequestHandlerSelectors.basePackage("com.revature.gambit")).paths(PathSelectors.any()).build();
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		log.info("Run init methods for all fallback classes");
+		TrainerService trainerService = context.getBean(TrainerService.class);
+		log.info("Size of list of trainers: "+trainerService.getAll().size());
+		
 	}
 }
