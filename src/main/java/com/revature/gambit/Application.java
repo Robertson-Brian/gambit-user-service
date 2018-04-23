0078d6e1b1d79ac49ccec93e179fff6a4a51196c
jeffrey reyes <jeffreyreyes12@gmail.com>
2018-04-23 16:25:47 -0400

    finnished setting up messaging and tests for messaging.

--- src/main/java/com/revature/gambit/Application.java
+++ src/main/java/com/revature/gambit/Application.java
@@ -1,9 +1,15 @@
 package com.revature.gambit;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+
+import com.revature.gambit.entities.Trainer;
+import com.revature.gambit.messaging.Sender;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -14,7 +20,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @SpringBootApplication
 @EnableEurekaClient
-public class Application {
+public class Application implements CommandLineRunner{
+	
+	@Autowired
+	ApplicationContext context;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -24,5 +33,11 @@ public class Application {
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
 				.apis(RequestHandlerSelectors.basePackage("com.revature.gambit")).paths(PathSelectors.any()).build();
+	}
+	
+	@Override
+	public void run(String... arg0) throws Exception {
+//		Sender sender = context.getBean(Sender.class);
+//		sender.publish("test", new Trainer("test", "test", "testtest@gmail.com", "Mr"));
 	}
 }
