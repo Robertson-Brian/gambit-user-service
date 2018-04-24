1795980dc795417d602425210d153f3bd503eb48
Mark Fleres <markfleres@gmail.com>
2018-04-24 16:12:26 -0400

    Added template wiring to TrainerController & Added MessagingUtil to store topic strings

---
+++ src/main/java/com/revature/gambit/util/MessagingUtil.java
@@ -0,0 +1,10 @@
+package com.revature.gambit.util;
+
+public class MessagingUtil {
+	public static final String TOPIC_REGISTER_TRAINER = "trainer.register";
+	public static final String TOPIC_UPDATE_TRAINER = "trainer.update";
+	public static final String TOPIC_DELETE_TRAINER = "trainer.delete";
+	public static final String TOPIC_REGISTER_TRAINEE = "trainee.register";
+	public static final String TOPIC_UPDATE_TRAINEE = "trainee.update";
+	public static final String TOPIC_DELETE_TRAINEE = "trainee.delete";
+}
