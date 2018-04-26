ed537c2a1e22ac0f9bc49babbffd643502b16fb0
jeffrey reyes <jeffreyreyes12@gmail.com>
2018-04-26 11:11:28 -0400

    controller test don't need to extend SenderTest so I changed it back to normal

--- src/test/java/com/revature/gambit/controllers/TraineeControllerTest.java
+++ src/test/java/com/revature/gambit/controllers/TraineeControllerTest.java
@@ -9,11 +9,11 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
 
+import com.revature.gambit.GambitTest;
 import com.revature.gambit.entities.Trainee;
-import com.revature.gambit.messaging.SenderTest;
 import com.revature.gambit.services.TraineeService;
 
-public class TraineeControllerTest extends SenderTest {
+public class TraineeControllerTest extends GambitTest {
 	
 	@LocalServerPort
 	private int port;
