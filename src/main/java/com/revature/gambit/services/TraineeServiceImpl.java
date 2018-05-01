8cbc39bacd6d7db98eeecf4150da85561bed5365
jingyu <jingyu459510777@gmail.com>
2018-05-01 13:54:31 -0400

    remove sysout

--- src/main/java/com/revature/gambit/services/TraineeServiceImpl.java
+++ src/main/java/com/revature/gambit/services/TraineeServiceImpl.java
@@ -129,11 +129,7 @@ public class TraineeServiceImpl implements TraineeService {
 	@HystrixCommand(fallbackMethod="findByEmailFallBack")
 	public Trainee findByEmail(String email) {
 		log.debug("findByEmail: " + email);
-		System.out.println("This is not a fallback.Findbyemail:"+email);
-		//if(traineeRepository.findByEmail(email)!=null)
 			return traineeRepository.findByEmail(email);
-	//	else
-	//		return null;
 	}
 
 
@@ -182,7 +178,6 @@ public class TraineeServiceImpl implements TraineeService {
 
 	public Trainee findByEmailFallBack(String email){
 		log.debug("FallBack for find trainee by Email");
-        System.out.println("This is a fallback. Findbyemail:"+email);
 		return traineeList.stream()
 				.filter(
 						(trainee)->
