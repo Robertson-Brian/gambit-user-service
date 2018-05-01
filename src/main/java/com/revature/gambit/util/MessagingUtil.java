3bb7c1c63bea1dae1d92f2d90de75685df20b021
Mark Fleres <markfleres@gmail.com>
2018-05-01 13:12:47 -0400

    Added Kafka topics to MessagingUtil for User

--- src/main/java/com/revature/gambit/util/MessagingUtil.java
+++ src/main/java/com/revature/gambit/util/MessagingUtil.java
@@ -9,6 +9,9 @@ public class MessagingUtil {
 	public static final String TOPIC_UPDATE_TRAINEE = "trainee.update.t";
 	public static final String TOPIC_DELETE_TRAINEE = "trainee.delete.t";
 	public static final String TOPIC_PROMOTE_USER_TO_TRAINER = "trainer.promote.t";
+	public static final String TOPIC_REGISTER_USER = "user.register.t";
+	public static final String TOPIC_UPDATE_USER = "user.update.t";
+	public static final String TOPIC_DELETE_USER = "user.delete.t";
 	
 	private MessagingUtil(){
 		throw new IllegalStateException("Utility class");
