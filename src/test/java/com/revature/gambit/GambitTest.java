a948f669d85d896d9ab10349dadae1786efa1c58
palagnajr <peter.alagna@revature.com>
2018-04-30 20:39:19 -0400

    Added solution for embedding microservice systems.

--- src/test/java/com/revature/gambit/GambitTest.java
+++ src/test/java/com/revature/gambit/GambitTest.java
@@ -1,8 +1,11 @@
 package com.revature.gambit;
 
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.rule.KafkaEmbedded;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -17,7 +20,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Sql(scripts = "/data-h2.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
 @Sql(scripts = "/tear-down.sql", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
+@DirtiesContext
 public class GambitTest {
+	
+	@ClassRule
+	public static KafkaEmbedded embeddedKafka =
+			new KafkaEmbedded(1, true, 1);
 	
 	@Test
 	public void run() {}
